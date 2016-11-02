49e40b7539d3bbde845287656f393f71e73ef153
Grigory Kislin <javawebinar@yandex.ru>
2016-11-03 00:42:24 +0300

    9_11_auth_via_user_service

--- src/main/java/ru/javawebinar/topjava/AuthorizedUser.java
+++ src/main/java/ru/javawebinar/topjava/AuthorizedUser.java
@@ -1,24 +1,56 @@
 package ru.javawebinar.topjava;
 
-import ru.javawebinar.topjava.model.BaseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.to.UserTo;
 import ru.javawebinar.topjava.util.UserUtil;
+
+import static java.util.Objects.requireNonNull;
 
 /**
  * GKislin
  * 06.03.2015.
  */
-public class AuthorizedUser {
-    public static int id = BaseEntity.START_SEQ;
+public class AuthorizedUser extends org.springframework.security.core.userdetails.User {
+    private static final long serialVersionUID = 1L;
 
-    public static int id() {
-        return id;
+    private final UserTo userTo;
+
+    public AuthorizedUser(User user) {
+        super(user.getEmail(), user.getPassword(), user.isEnabled(), true, true, true, user.getRoles());
+        this.userTo = UserUtil.asTo(user);
     }
 
-    public static void setId(int id) {
-        AuthorizedUser.id = id;
+    public static AuthorizedUser safeGet() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth == null) {
+            return null;
+        }
+        Object principal = auth.getPrincipal();
+        return (principal instanceof AuthorizedUser) ? (AuthorizedUser) principal : null;
+    }
+
+    public static AuthorizedUser get() {
+        AuthorizedUser user = safeGet();
+        requireNonNull(user, "No authorized user found");
+        return user;
+    }
+
+    public static int id() {
+        return get().userTo.getId();
     }
 
     public static int getCaloriesPerDay() {
-        return UserUtil.DEFAULT_CALORIES_PER_DAY;
+        return get().userTo.getCaloriesPerDay();
+    }
+
+    public UserTo getUserTo() {
+        return userTo;
+    }
+
+    @Override
+    public String toString() {
+        return userTo.toString();
     }
 }
--- src/main/java/ru/javawebinar/topjava/to/UserTo.java
+++ src/main/java/ru/javawebinar/topjava/to/UserTo.java
@@ -2,10 +2,16 @@ package ru.javawebinar.topjava.to;
 
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
+import org.hibernate.validator.constraints.Range;
+import ru.javawebinar.topjava.util.UserUtil;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.io.Serializable;
 
-public class UserTo {
+public class UserTo implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private Integer id;
 
     @NotEmpty
@@ -18,14 +24,19 @@ public class UserTo {
     @Size(min = 5, max = 64, message = " must between 5 and 64 characters")
     private String password;
 
+    @Range(min = 100, max = 5000)
+    @NotNull(message = " must not be empty")
+    private Integer caloriesPerDay = UserUtil.DEFAULT_CALORIES_PER_DAY;
+
     public UserTo() {
     }
 
-    public UserTo(Integer id, String name, String email, String password) {
+    public UserTo(Integer id, String name, String email, String password, int caloriesPerDay) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.password = password;
+        this.caloriesPerDay = caloriesPerDay;
     }
 
     public Integer getId() {
@@ -64,12 +75,17 @@ public class UserTo {
         return id == null;
     }
 
+    public int getCaloriesPerDay() {
+        return caloriesPerDay;
+    }
+
     @Override
     public String toString() {
         return "UserTo{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", email='" + email + '\'' +
+                ", caloriesPerDay='" + caloriesPerDay + '\'' +
                 '}';
     }
 }
