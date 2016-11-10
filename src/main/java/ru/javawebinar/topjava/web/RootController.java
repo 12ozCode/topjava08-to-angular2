2e3e8da100157af5df94bac9507e40cff19fcf4a
Grigory Kislin <javawebinar@yandex.ru>
2016-11-10 07:52:00 +0300

    10_04_secure_tag_annotation

--- src/main/java/ru/javawebinar/topjava/web/RootController.java
+++ src/main/java/ru/javawebinar/topjava/web/RootController.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava.web;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +20,8 @@ public class RootController {
         return "redirect:meals";
     }
 
+//    @Secured("ROLE_ADMIN")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/users")
     public String users() {
         return "users";
